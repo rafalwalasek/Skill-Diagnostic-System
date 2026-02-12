270282474002555d1ece650ab5c3cd6bc2963778
Rafal Walasek <magician1590@gmail.com>
2026-02-12 10:03:36 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/rafal/skilldiagnosticsystem/SkillDiagnosticSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rafal.skilldiagnosticsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillDiagnosticSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillDiagnosticSystemApplication.class, args);
+	}
+
+}
