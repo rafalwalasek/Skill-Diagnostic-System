5ff9fbd42a30ae8dc5668cde01beb8e2995bff1b
Rafal Walasek <rafal_walasek@interia.pl>
2026-06-05 16:05:24 +0200

    start quizu - test

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,11 +1,11 @@
 let questions = [];
 fetch("/quiz/questions?st=JAVA&diff=EASY")
     .then(res => {
-        console.log("STATUS:", res.status);
+        // console.log("STATUS:", res.status);
         return res.json();
     })
     .then(data => {
-        console.log("DATA:", data);
+        // console.log("DATA:", data);
         questions = data;
         renderQuestion();
     });
@@ -19,101 +19,70 @@ function renderQuestion() {
     quiz.innerHTML = `
         <div class="question-box">
             <h3>Question ${currentIndex + 1} / ${questions.length}</h3>
-            <p>${currentQuestion.question}</p>
+            <p>${currentQuestion.content}</p>
 
             <div class="answers">
-                A
-                B
-                C
-                D
+                ${renderAnswer(currentQuestion.id, "A", currentQuestion.answerA)}
+                ${renderAnswer(currentQuestion.id, "B", currentQuestion.answerB)}
+                ${renderAnswer(currentQuestion.id, "C", currentQuestion.answerC)}
+                ${renderAnswer(currentQuestion.id, "D", currentQuestion.answerD)}
             </div>
+            <div class="navigation">
+                <button onclick="prevQuestion()">Previous</button>
+                <button onclick="nextQuestion()">Next</button>
+            </div>
+            <button class="submit-btn" onclick="submitQuiz()">
+                Finish Quiz
+            </button>
         </div>
     `;
 }
+    let answers = {};
+    function renderAnswer(id, key, text) {
+        const checked = answers[id] === key ? "checked" : "";
 
+        return `
+            <label>
+                <input type="radio"
+                    name="q${id}"
+                    value="${key}"
+                    ${checked}
+                    onchange="saveAnswer(${id}, '${key}')">
+                ${key}. ${text}
+            </label>
+        `;
+    }
+        function saveAnswer(id, answer) {
+            answers[id] = answer;
+        }
+    function nextQuestion() {
+        if (currentIndex < questions.length - 1) {
+            currentIndex++;
+            renderQuestion();
+        }
+    }
+    function prevQuestion() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            renderQuestion();
+        }
+    }
+    function submitQuiz() {
+        const userAnswerList = Object.keys(answers).map(qId => ({
+            questionId: parseInt(qId),
+            answer: answers[qId]
+        }));
 
-// ${renderAnswer(currentQuestion.id, "A", currentQuestion.answerA)}
-//                 ${renderAnswer(currentQuestion.id, "B", currentQuestion.answerB)}
-//                 ${renderAnswer(currentQuestion.id, "C", currentQuestion.answerC)}
-//                 ${renderAnswer(currentQuestion.id, "D", currentQuestion.answerD)}
-// let questions = [];
-// let currentIndex = 0;
-// let answers = {};
+    //     fetch('/quiz/submit', {
+    //         method: 'POST',
+    //         headers: {'Content-Type': 'application/json'},
+    //         body: JSON.stringify({ userAnswerList })
+    //     })
+    //     .then(res => res.json())
+    //     .then(result => {
+    //         document.getElementById("result").innerText =
+    //             "Twój wynik: " + result;
+    //     });
 
-// // LOAD QUESTIONS
-// fetch('/quiz/questions')
-//     .then(res => res.json())
-//     .then(data => {
-//         questions = data;
-//         renderQuestion();
-//     });
-
-// function renderQuestion() {
-//     const q = questions[currentIndex];
-//     if (!q) return;
-
-//     document.getElementById("quiz").innerHTML = `
-//         <div class="question-box">
-//             <h3>Question ${currentIndex + 1} / ${questions.length}</h3>
-//             <p>${q.question}</p>
-
-//             <div class="answers">
-//                 ${renderAnswer(q.id, "A", q.answerA)}
-//                 ${renderAnswer(q.id, "B", q.answerB)}
-//                 ${renderAnswer(q.id, "C", q.answerC)}
-//                 ${renderAnswer(q.id, "D", q.answerD)}
-//             </div>
-//         </div>
-//     `;
-// }
-
-// function renderAnswer(id, key, text) {
-//     const checked = answers[id] === key ? "checked" : "";
-
-//     return `
-//         <label>
-//             <input type="radio"
-//                    name="q${id}"
-//                    value="${key}"
-//                    ${checked}
-//                    onchange="saveAnswer(${id}, '${key}')">
-//             ${key}. ${text}
-//         </label>
-//     `;
-// }
-
-// function saveAnswer(id, answer) {
-//     answers[id] = answer;
-// }
-
-// function nextQuestion() {
-//     if (currentIndex < questions.length - 1) {
-//         currentIndex++;
-//         renderQuestion();
-//     }
-// }
-
-// function prevQuestion() {
-//     if (currentIndex > 0) {
-//         currentIndex--;
-//         renderQuestion();
-//     }
-// }
-
-// function submitQuiz() {
-//     const userAnswerList = Object.keys(answers).map(qId => ({
-//         questionId: parseInt(qId),
-//         answer: answers[qId]
-//     }));
-
-//     fetch('/quiz/submit', {
-//         method: 'POST',
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify({ userAnswerList })
-//     })
-//     .then(res => res.json())
-//     .then(result => {
-//         document.getElementById("result").innerText =
-//             "Twój wynik: " + result;
-//     });
-// }
+        alert(JSON.stringify(userAnswerList, null, 2));
+    }
