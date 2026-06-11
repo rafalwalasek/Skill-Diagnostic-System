5ae02fd78cc3567cb0715c96780e24843ed58e2f
Rafal Walasek <rafal_walasek@interia.pl>
2026-06-11 13:57:50 +0200

    praca nad pokazywaniem szczegolow kategorii cz 1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,7 +11,7 @@
 -- (3, 'Projektowanie aplikacji'),
 -- (4, 'Referring');
 
--- INSERT INTO question(content, answerA, answerB, answerC, answerD, correct_answer, difficulty_level, category) VALUES
+INSERT INTO question(subtopic_id, content, answerA, answerB, answerC, answerD, correct_answer, difficulty_level) VALUES
 -- ('Jaka jest różnica między obiektem wartości (Value Object) a encją (Entity) w kontekście DDD?', 'VO jest niemutowalny (ang. immutable), a encja jest mutowalna (ang. mutable)', 'VO nie posiada identyfikatora a encja posiada', 'Cykl życia VO może zależeć od encji, ale cykl życia encji nie może zależeć od VO', 'Wszystkie odpowiedzi są poprawne', 'D', 'HARD', 'PROJEKTOWANIE_APLIKACJI'),
 -- ('Który typ danych w Javie służy do przechowywania liczb całkowitych?', 'double', 'int', 'char', 'boolean', 'B', 'EASY', 'JAVA'),
 -- ('Która pętla wykona się co najmniej raz niezależnie od warunku?', 'for', 'while', 'do-while', 'foreach', 'C', 'EASY', 'JAVA'),
@@ -19,5 +19,5 @@
 -- ('Co oznacza słowo kluczowe final dla zmiennej w Javie?', 'Zmienna może zmieniać wartość tylko raz po każdej metodzie.', 'Zmienna nie może zostać ponownie przypisana po inicjalizacji.', 'Zmienna jest dostępna tylko w klasie.', 'Zmienna jest automatycznie statyczna.', 'B', 'EASY', 'JAVA'),
 -- ('Która kolekcja w Javie przechowuje unikalne elementy?', 'List', 'ArrayList', 'Set', 'LinkedList', 'C', 'EASY', 'JAVA'),
 -- ('Do czego służy adnotacja @Override?', 'Do tworzenia nowej klasy.', 'Do oznaczenia, że metoda nadpisuje metodę z klasy nadrzędnej lub interfejsu.', 'Do ukrywania metod przed innymi klasami.', 'Do automatycznego tworzenia getterów i setterów.', 'B', 'EASY', 'JAVA'),
--- ('Napastnik wcześniej już napomniany w zawodach nadmiernie fetuje zdobycie bramki (zakrywa twarz maską) po tym, jak udało mu się wyrównać wynik meczu. Jaką decyzje podejmie sędzia jeżeli uzna on wskazanie swojego asystenta, na podstawie którego bramka zostanie nieuznana z tytułu wcześniej zaistniałego spalonego?', '+', 'P', 'P + ++', 'S', 'C', 'MEDIUM', 'REFERRING');
+-- (4, 'Napastnik wcześniej już napomniany w zawodach nadmiernie fetuje zdobycie bramki (zakrywa twarz maską) po tym, jak udało mu się wyrównać wynik meczu. Jaką decyzje podejmie sędzia jeżeli uzna on wskazanie swojego asystenta, na podstawie którego bramka zostanie nieuznana z tytułu wcześniej zaistniałego spalonego?', '+', 'P', 'P + ++', 'S', 'C', 'MEDIUM');
 -- ('', '', '', '', '', '', '', '');
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,7 +3,7 @@ async function loadTopics() {
     const res = await fetch("/quiz/topics");
     topicsAndSubtopics = await res.json();
 
-    console.log(topicsAndSubtopics);
+    //console.log(topicsAndSubtopics);
     renderTopics(topicsAndSubtopics);
 }
 
@@ -55,70 +55,62 @@ function renderTopics(topicsAndSubtopics) {
                 `).join("")}
             </div>
         `;
-
-        document.querySelector(".subtopic-item")
-            .addEventListener("click", (e) => {
-                if (e.target.classList.contains("diff-btn")) {
-                    //console.log("klik:", e.target.textContent);
-                    const row = e.target.closest(".subtopic-item-row");
-                    console.log("klik:", e.target.dataset.subtopicId);
-                    difficultyButtons(row, e.target.dataset.subtopicId, e.target.dataset.diff);
-                }
-            });
     }// END Subtopics
         // Details
-        function difficultyButtons(row, subtopicId, diff) {
-            const existingDetails = row.querySelector(".details-block");
+        document.addEventListener("click", (e) => {
+            console.log("TARGET:", e.target);
+            console.log("ROW:", e.target.closest(".subtopic-item-row"));
 
-            if (existingDetails) {
-                existingDetails.remove();
+            if (e.target.classList.contains("diff-btn")) {
+                const row = e.target.closest(".subtopic-item-row");
+                const subtopicId = e.target.dataset.subtopicId;
+                const diff = e.target.dataset.diff;
+
+                renderSubtopicOverview(row, subtopicId, diff);
             }
+        });
+            function renderSubtopicOverview(row, subtopicId, difficulty) {
+                const existingDetails = row.querySelector(".details-block");
+                const id = Number(subtopicId);
 
-            const details = document.createElement("div");
-            details.classList.add("details-block");
+                // console.log(existingDetails);
 
-            const btn = document.createElement("button");
-            btn.textContent = "Start Quiz";
-            btn.classList.add("button-quiz");
+                if (existingDetails) {
+                    existingDetails.remove();
+                }
 
-            details.innerHTML = `
-                <hr>
-                <div class="stats">
-                    <div class="stat">📘 Questions: <span class="count">0</span></div>
-                    <div class="stat">🔁 Attempts: 0</div>
-                    <div class="stat">🏷 Status: Not progress</div>
-                </div>
-                <div class="master-progress">
-                    <div class="temat">
-                        <span>📊 Mastery</span>
-                        <span>10%</span>
+                const details = document.createElement("div");
+                details.classList.add("details-block");
+
+                details.innerHTML = `
+                    <hr>
+                    <div class="stats">
+                        <div class="stat">📘 Questions: <span class="count">0</span></div>
+                        <div class="stat">🔁 Attempts: 0</div>
+                        <div class="stat">🏷 Status: Not progress</div>
                     </div>
-                    <div class="progress">
-                        <div class="progress-bar" style="width: 10%"></div>
+                    <div class="master-progress">
+                        <div class="temat">
+                            <span>📊 Mastery</span>
+                            <span>10%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 10%"></div>
+                        </div>
                     </div>
-                </div>
-            `;
+                `;
 
-            btn.onclick = () => {
-                // alert("Quiz start: " + st + " | " + diff);
-                // window.open("quiz.html");
-                window.open("quiz.html?st=JAVA&diff=EASY");
-            };
-            
-            details.appendChild(btn);
-            row.appendChild(details);
+                row.appendChild(details);
 
-            getQuestionCount(row, st, diff);
-        }// END Details
+                getQuestionCount(row, subtopicId, difficulty);
+            }// END Details
 
 loadTopics();
 
 // =================================================================================================
 // zliczanie pytan z konkretnego poziomu
-function getQuestionCount(row, st, diff) {
-    const subtop = st.toUpperCase();
-
-    fetch(`/quiz/questionCount?st=${subtop}&diff=${diff}`)
+function getQuestionCount(row, subtopicId, difficulty) {
+    fetch(`/quiz/questionCount?subtopicId=${subtopicId}&diff=${difficulty}`)
     .then(response => response.json())
     .then(data => {
         //console.log(data);
@@ -127,3 +119,49 @@ function getQuestionCount(row, st, diff) {
     })
     .catch(error => console.error("Błąd:", error));
 }
+
+
+
+// function difficultyButtons(row, subtopicId, diff) {
+//             const existingDetails = row.querySelector(".details-block");
+
+//             if (existingDetails) {
+//                 existingDetails.remove();
+//             }
+
+//             const details = document.createElement("div");
+//             details.classList.add("details-block");
+
+//             const btn = document.createElement("button");
+//             btn.textContent = "Start Quiz";
+//             btn.classList.add("button-quiz");
+
+            // details.innerHTML = `
+            //     <hr>
+            //     <div class="stats">
+            //         <div class="stat">📘 Questions: <span class="count">0</span></div>
+            //         <div class="stat">🔁 Attempts: 0</div>
+            //         <div class="stat">🏷 Status: Not progress</div>
+            //     </div>
+            //     <div class="master-progress">
+            //         <div class="temat">
+            //             <span>📊 Mastery</span>
+            //             <span>10%</span>
+            //         </div>
+            //         <div class="progress">
+            //             <div class="progress-bar" style="width: 10%"></div>
+            //         </div>
+            //     </div>
+            // `;
+
+            // btn.onclick = () => {
+                // alert("Quiz start: " + st + " | " + diff);
+                // window.open("quiz.html");
+        //         window.open("quiz.html?st=JAVA&diff=EASY");
+        //     };
+            
+        //     details.appendChild(btn);
+        //     row.appendChild(details);
+
+        //     getQuestionCount(row, st, diff);
+        // }
